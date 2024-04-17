61e6942f24f9b759072a77a8670c5228f902d6c5
lontonggg <zadareyhan@gmail.com>
2024-04-17 17:32:54 +0700

    Fixed font size on welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-32 gap-10">
-      <p className="text-5xl">Welcome to <b>Snackscription</b>!</p>
-      <p className="text-3xl text-gray-500">This website is currently under development.</p>
+      <p className="text-5xl text-center">Welcome to <b>Snackscription</b>!</p>
+      <p className="text-xl text-center text-gray-500">This website is currently under development.</p>
     </main>
   );
 }
