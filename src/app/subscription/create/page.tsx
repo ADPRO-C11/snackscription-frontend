e555a34e6748976a8b82a08da33363143b1fda18
lontonggg <zadareyhan@gmail.com>
2024-05-08 13:38:28 +0700

    [FEAT] Create page to create subscriptions

---
+++ src/app/subscription/create/page.tsx
@@ -0,0 +1,12 @@
+import { CreateSubscriptionForm } from '@/components/subscription/CreateSubscriptionForm'
+import React from 'react'
+
+export default function CreateSubscription() {
+  return (
+    <>  
+        <div className='p-20'>
+          <CreateSubscriptionForm />
+        </div>
+    </>
+  )
+}
