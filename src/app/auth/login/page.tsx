d2e34d41b44f67d2ca9444417f102182cf4223d1
lontonggg <zadareyhan@gmail.com>
2024-05-20 02:25:25 +0700

    [FEAT] Create login and register page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import React from 'react'
+import Image from 'next/image'
+import { LoginForm } from '@/components/authentication/LoginForm'
+
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-32 gap-10 bg-black bg-opacity-50">
+    <Image 
+        className='blur-sm'
+        src={"/static/images/Snacks.jpg"}
+        alt="Image of Snacks"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+            objectFit: 'cover',
+            zIndex: -1
+        }}
+    />
+    <div className="bg-white w-[300px] sm:w-[350px] py-8 sm:py-10 rounded-2xl shadow-xl flex flex-col gap-10 items-center justify-center">
+        <LoginForm />
+    </div>
+    </main>
+  )
+}
