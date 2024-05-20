48d687e36056cd9521fcd745b8f115b370d579e9
lontonggg <zadareyhan@gmail.com>
2024-05-20 13:07:39 +0700

    [FIX] Configure proxy to handle HTTP API requests via HTTPS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import AuthenticationService from "@/components/authentication/AuthenticationService";
 import { getCookie, hasCookie } from "cookies-next";
 import Image from "next/image";
@@ -21,55 +21,61 @@ export default function Home() {
     username: '',
     enabled: true
   });
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     if (!hasCookie('token')) {
+      setIsAuthenticated(false);
       router.push('/welcome');
+    } else {
+      setIsAuthenticated(true);
+      getUserProfile();
     }
   }, [router]);
 
   const handleLogout = () => {
     AuthenticationService.logout();
-    toast.success('Logout Success')
+    toast.success('Logout Success');
+    setIsAuthenticated(false);
     router.push('/welcome');
   }
 
   const getUserProfile = async () => {
-    const token = getCookie('token')
-    try{
-        if(token){
-          const userData = await AuthenticationService.getProfile(token)
-          setUser(userData.user);
-        } else{
-          
-        }
+    const token = getCookie('token');
+    try {
+      if (token) {
+        const userData = await AuthenticationService.getProfile(token);
+        setUser(userData.user);
+      }
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
   }
-  if(user.id == ''){
-    getUserProfile();
-  }
-  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-10">
-        <Image 
-          src={"/static/images/Snacks.jpg"}
-          alt="Image of Snacks"
-          quality={100}
-          fill
-          sizes="100vw"
-          style={{
-            objectFit: 'cover',
-            zIndex: -1
-          }}
-        />
-        <div className="bg-white py-10 w-[300px] sm:w-[500px] rounded-3xl shadow-xl flex flex-col gap-10 items-center justify-center">
-          <p className="sm:text-3xl text-center">Welcome, {user.name}!</p>
-          <p className="sm:text-xl text-center text-gray-500 px-10">Sorry, this website is currently under development.</p>
-          <button onClick={handleLogout} className="bg-orange-300 w-[150px] py-3 rounded-md text-sm sm:text-lg font-medium focus:bg-orange-400 hover:bg-orange-400">Logout</button>
-        </div>
+      <Image 
+        src={"/static/images/Snacks.jpg"}
+        alt="Image of Snacks"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: 'cover',
+          zIndex: -1
+        }}
+      />
+      <div className="bg-white py-10 w-[300px] sm:w-[500px] rounded-3xl shadow-xl flex flex-col gap-10 items-center justify-center">
+        {isAuthenticated ? (
+          <>
+            <p className="sm:text-3xl text-center">Welcome, {user.name}!</p>
+            <p className="sm:text-xl text-center text-gray-500 px-10">Sorry, this website is currently under development.</p>
+            <button onClick={handleLogout} className="bg-orange-300 w-[150px] py-3 rounded-md text-sm sm:text-lg font-medium focus:bg-orange-400 hover:bg-orange-400">Logout</button>
+          </>
+        ) : (
+          <p className="sm:text-3xl text-center">Loading...</p>
+        )}
+      </div>
     </main>
   );
 }
