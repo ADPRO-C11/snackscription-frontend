4b495f2dd68c34f7ce5cc796f7d39a85821547aa
lontonggg <zadareyhan@gmail.com>
2024-05-24 13:06:54 +0700

    [FEAT] Created Navbar and Create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import AuthenticationService from "@/components/authentication/AuthenticationService";
+import { Navbar } from "@/components/common/Navbar";
+import { AboutUs } from "@/components/home/AboutUs";
+import { MainImage } from "@/components/home/MainImage";
+import { OurPlan } from "@/components/home/OurPlan";
 import { getCookie, hasCookie } from "cookies-next";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
 
 export default function Home() {
   const router = useRouter();
@@ -33,13 +35,6 @@ export default function Home() {
     }
   }, [router]);
 
-  const handleLogout = () => {
-    AuthenticationService.logout();
-    toast.success('Logout Success');
-    setIsAuthenticated(false);
-    router.push('/welcome');
-  }
-
   const getUserProfile = async () => {
     const token = getCookie('token');
     try {
@@ -53,29 +48,27 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-10">
-      <Image 
-        src={"/static/images/Snacks.jpg"}
-        alt="Image of Snacks"
-        quality={100}
-        fill
-        sizes="100vw"
-        style={{
-          objectFit: 'cover',
-          zIndex: -1
-        }}
-      />
-      <div className="bg-white py-10 w-[300px] sm:w-[500px] rounded-3xl shadow-xl flex flex-col gap-10 items-center justify-center">
-        {isAuthenticated ? (
-          <>
-            <p className="sm:text-3xl text-center">Welcome, {user.name}!</p>
-            <p className="sm:text-xl text-center text-gray-500 px-10">Sorry, this website is currently under development.</p>
-            <button onClick={handleLogout} className="bg-orange-300 w-[150px] py-3 rounded-md text-sm sm:text-lg font-medium focus:bg-orange-400 hover:bg-orange-400">Logout</button>
-          </>
-        ) : (
-          <p className="sm:text-3xl text-center">Loading...</p>
-        )}
-      </div>
+    <main className="pt-16">
+      {isAuthenticated ? (
+        <>
+          <Navbar username={user.name} />
+          <MainImage />
+          <OurPlan />
+          <AboutUs />
+          <div className="flex">
+            <div>
+              <p></p>
+            </div>
+          </div>
+        </>
+        
+      ) : (
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+          <div className="bg-white py-10 w-[300px] sm:w-[500px] rounded-3xl shadow-xl flex flex-col gap-10 items-center justify-center">
+            <p className="sm:text-3xl text-center">Loading...</p>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
---
+++ src/components/home/OurPlan.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const OurPlan = () => {
+  return (
+    <div className='pb-16' style={{ backgroundColor: "#FAF3E0" }}>
+      <div className='flex flex-col justify-center items-center p-16 px-32 gap-16'>
+        <h1 className='text-5xl font-extrabold text-center'>Our Plans</h1>
+        <div className='grid grid-cols-3 gap-24'>
+          <div className='border border-gray-300 rounded-2xl p-20 bg-orange-100 flex flex-col items-center justify-center gap-3 shadow-xl transform transition duration-300 hover:scale-105 hover:shadow-2xl'>
+            <h1 className='text-center text-4xl font-semibold'>Monthly</h1>
+            <br></br>
+            <p className='text-justify text-wrap text-gray-500'>Receive a box of gourmet snacks every month, perfect for those who love trying new treats regularly.</p>
+          </div>
+          <div className='border border-gray-300 rounded-2xl p-20 bg-orange-100 flex flex-col items-center justify-center gap-3 shadow-xl transform transition duration-300 hover:scale-105 hover:shadow-2xl'>
+            <h1 className='text-center text-4xl font-semibold'>Quarterly</h1>
+            <br></br>
+            <p className='text-justify text-wrap text-gray-500'>Get a curated snack box every three months, ideal for enjoying snacks over a more extended period.</p>
+          </div>
+          <div className='border border-gray-300 rounded-2xl p-20 bg-orange-100 flex flex-col items-center justify-center gap-3 shadow-xl transform transition duration-300 hover:scale-105 hover:shadow-2xl'>
+            <h1 className='text-center text-4xl font-semibold'>Semi-Annual</h1>
+            <br></br>
+            <p className='text-justify text-wrap text-gray-500'>Receive a large selection of premium snacks every six months, perfect for stocking up and savoring over a longer time.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
