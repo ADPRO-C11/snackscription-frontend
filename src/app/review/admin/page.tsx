e4feff0daed85a403d36e2175b16d1a95d1645be
asteriskzie <ester.gracia.10@gmail.com>
2024-05-26 20:55:01 +0700

    Add review admin page

---
+++ src/app/review/admin/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { ReviewAdmin } from "@/components/modules/ReviewModule/ReviewAdmin"
+
+const AdminPage = () => {
+    return <ReviewAdmin />
+}
+
+export default AdminPage;
--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -2,6 +2,12 @@
 
 import { ReviewModule } from "@/components/modules/ReviewModule"
 
-const Review = () => <ReviewModule />
+const Review = () => {
+    const id = {
+        subsbox: "1",
+        user: "asteriskzie"
+    }
+    return <ReviewModule subsbox="1" user="asteriskzie"/>
+}
 
 export default Review;
