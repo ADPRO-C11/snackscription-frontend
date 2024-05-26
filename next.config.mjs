d68ab51a1197ff12d9e45270ef33627a219640a1
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-05-27 01:39:32 +0700

    TERAKHIR

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/auth/:path*',
-            destination: `${process.env.NEXT_PUBLIC_AUTH_API}/:path*`,
-          },
-        ];
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: `${process.env.NEXT_PUBLIC_AUTH_API}/:path*`,
       },
-      async headers() {
-        return [
-          {
-            source: "/api/auth/:path*",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ],
-          },
-        ];
+      {
+        source: '/api/subscription/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SUBSCRIPTION_API}/:path*`,
       },
+      {
+        source: '/api/subscription-box/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SUBSCRIPTION_BOX_API}/:path*`,
+      },
+      {
+        source: '/api/review/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_REVIEW_URL}/:path*`,
+      },
+      {
+        source: '/api/subscription-admin/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SUBSCRIPTION_ADMIN_API}/:path*`,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
